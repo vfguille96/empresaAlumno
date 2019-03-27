41fc6a2ad2f97eb6fe1ad18da72ff3f2c28e99ee
Guillermo <vfguille@gmail.com>
2019-03-27 20:32:38 +0100

    Commit inicial

---
+++ php/app.php
@@ -0,0 +1,38 @@
+<?php
+include_once 'dao.php';
+
+class App
+{
+    protected $dao;
+
+    function __construct()
+    {
+        $this->dao = new Dao();
+    }
+
+    function getDao()
+    {
+        return $this->dao;
+    }
+
+    function show_head($titulo)
+    {
+        print " <!DOCTYPE html>
+        <html lang=\"es\">
+            <head>
+                <meta charset=\"utf-8\"/>
+                <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
+                <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+                <title>" . $titulo . "</title>
+                <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css\">
+                <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js\"></script>
+                <script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js\"></script>
+                <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js\"></script>
+            </head>
+            <body>
+            ";
+
+    }
+}
+
+?>
